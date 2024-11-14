e75468104d0691e399cef16d35011b367fc2936e
rjmaligaya <rjmaligaya@gmail.com>
2024-11-14 12:06:18 -0500

    Update Flask to use PORT environment variable

--- app.py
+++ app.py
@@ -106,4 +106,6 @@ def download_template():
     return send_file(template_path, as_attachment=True, download_name='COPUS_template.xlsx')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
